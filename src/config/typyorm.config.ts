ded53d13a7976dc2d992635e37a1a455238a2d11
Saulo Silva <saulo@s10design.com.br>
2019-10-18 14:53:16 -0300

    Conexao com docker

--- src/config/typyorm.config.ts
+++ src/config/typyorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: '127.0.0.1',
+  host: '0.0.0.0',
   port: 3306,
   username: 'root',
   password: 'root',
