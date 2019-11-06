684860a0e11a94ea33be8e168ea6f7085d809cee
Saulo Silva <saulo@s10design.com.br>
2019-11-06 17:42:54 -0300

    Ajuste código

--- src/tasks/get-tasks-filter.dto.ts
+++ src/tasks/get-tasks-filter.dto.ts
@@ -1,4 +1,4 @@
-import { TaskStatus } from "./task-status.enum"
+import { TaskStatus } from './task-status.enum';
 import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 
 export class GetTasksFilterDto {
